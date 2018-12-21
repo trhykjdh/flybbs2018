c60f21f9e8a69292096acad35ac59e8abaa04a22
trhykjdh <401158620@qq.com>
2018-12-21 15:44:38 +0800

    new 修改帖子和点赞

--- src/main/java/com/neusoft/controller/JieController.java
+++ src/main/java/com/neusoft/controller/JieController.java
@@ -1,12 +1,7 @@
 package com.neusoft.controller;
 import com.alibaba.fastjson.JSON;
-import com.neusoft.domain.Comment;
-import com.neusoft.domain.Topic;
-import com.neusoft.domain.TopicCategory;
-import com.neusoft.domain.User;
-import com.neusoft.mapper.CommentMapper;
-import com.neusoft.mapper.TopicCategoryMapper;
-import com.neusoft.mapper.TopicMapper;
+import com.neusoft.domain.*;
+import com.neusoft.mapper.*;
 import com.neusoft.response.RegRespObj;
 import com.neusoft.util.StringDate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,12 +14,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Controller
-@RequestMapping("jie")
+@RequestMapping("jie")//发帖回复
 public class JieController {
     @Autowired
     TopicCategoryMapper topicCategoryMapper;
@@ -32,12 +27,21 @@ public class JieController {
     TopicMapper topicMapper;
     @Autowired
     CommentMapper commentMapper;
-
-    @RequestMapping("add")
-    public ModelAndView add() {
+    @Autowired
+    UserMapper userMapper;
+    @Autowired
+    UserMessageMapper userMessageMapper;
+    @RequestMapping("add/{tid}")
+    public ModelAndView add(@PathVariable Integer tid) {
         List<TopicCategory> topicCategoryList = topicCategoryMapper.getAllCategories();
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("categories", topicCategoryList);
+        modelAndView.addObject("tid",tid);
+        if(tid > 0)
+        {
+            Topic topic = topicMapper.selectByPrimaryKey(tid);
+            modelAndView.addObject("topic",topic);
+        }
         modelAndView.setViewName("jie/add");
         return modelAndView;
     }
@@ -48,24 +52,61 @@ public class JieController {
         HttpSession httpSession = request.getSession();
         User user = (User) httpSession.getAttribute("userinfo");
         topic.setUserid(user.getId());//评论的用户ID    评论时间
-        topic.setCreateTime(new Date());
-        int result = topicMapper.insertSelective(topic);
-        if (result > 0) {//写状态写地址
-            regRespObj.setStatus(0);
-            regRespObj.setAction(request.getServletContext().getContextPath() + "/");
-            response.getWriter().println(JSON.toJSONString(regRespObj));
-        }
-        return;
+      if(topic.getId() == 0)//新增
+      {
+          topic.setCreateTime(new Date());
+          if (topic.getKissNum() < user.getKissNum()) {
+              int result = topicMapper.insertSelective(topic);
+              user.setKissNum(user.getKissNum() - topic.getKissNum());
+              userMapper.updateByPrimaryKeySelective(user);
+              //写状态写地址
+              if (result > 0) {
+                  regRespObj.setStatus(0);
+                  regRespObj.setAction(request.getServletContext().getContextPath() + "/");
+              }
+          } else {
+              regRespObj.setStatus(1);
+              regRespObj.setMsg("飞吻不足");
+          }
+      }else {
+          int result = topicMapper.updateByPrimaryKeySelective(topic);
+          if(result > 0)
+          {
+              regRespObj.setStatus(0);
+              regRespObj.setAction(request.getServletContext().getContextPath() + "/");
+          }
+      }
+
+        response.getWriter().println(JSON.toJSONString(regRespObj));
     }
     @RequestMapping("detail/{tid}")//帖子
-    public ModelAndView detail(@PathVariable Integer tid)
+    public ModelAndView detail(@PathVariable Integer tid,HttpSession httpSession)
     {
+        //帖子的阅读数量加一
+        Topic topic = topicMapper.selectByPrimaryKey(tid);
+        topic.setViewTimes(topic.getViewTimes() + 1);
+        topicMapper.updateByPrimaryKeySelective(topic);
+
         ModelAndView modelAndView = new ModelAndView();
         Map<String,Object> map = topicMapper.getTopicInfo(tid);
         Date date = (Date)map.get("create_time");
         String strDate = StringDate.getStringDate(date);
         map.put("create_time",strDate);
-        List<Map<String,Object>> mapList = commentMapper.getCommentsByTopicID(tid);//回复
+        //3.该帖的评论的赞的信息
+        User user = (User)httpSession.getAttribute("userinfo");
+
+        Map<String,Object> params = new HashMap<>();
+        params.put("topicid",tid);
+        if(user == null)
+        {
+            params.put("userid",0);
+        }
+        else
+        {
+            params.put("userid",user.getId());
+        }
+
+        List<Map<String,Object>> mapList = commentMapper.getCommentsByTopicID(params);//回复
         for(Map<String,Object> map2 : mapList)
         {
             Date date2 = (Date)map2.get("comment_time");
@@ -89,13 +130,64 @@ public class JieController {
             comment.setCommentTime(new Date());
             commentMapper.insertSelective(comment);
             regRespObj.setStatus(0);
+
+            //更新topic的评论数字段
+            Topic topic = topicMapper.selectByPrimaryKey(comment.getTopicId());
+            int comment_num = topic.getCommentNum();
+            topic.setCommentNum(comment_num + 1);
+            topicMapper.updateByPrimaryKeySelective(topic);
+            //在消息表中添加一条消息
+            if(topic.getUserid() != user.getId())
+            {
+                UserMessage userMessage = new UserMessage();
+                userMessage.setCreateTime(new Date());
+                userMessage.setTopicId(comment.getTopicId());
+                userMessage.setMsgType(1);
+                userMessage.setTriggerMsgUserId(user.getId());
+                userMessage.setRecvMsgUserId(topic.getUserid());
+                userMessageMapper.insertSelective(userMessage);
+            }
+
+            //正则
+            Pattern pattern = Pattern.compile("@(.*?)\\s");
+            Matcher matcher = pattern.matcher(comment.getCommentContent());
+            Set<String> stringSet = new HashSet<>();
+            while (matcher.find()) {
+                String nickname = matcher.group(1);
+                stringSet.add(nickname);
+            }
+            for(String username : stringSet)
+            {
+                User user1 = userMapper.selectByNickname(username);
+                if(user1 != null)
+                {
+                    UserMessage userMessage = new UserMessage();
+                    userMessage.setCreateTime(new Date());
+                    userMessage.setTopicId(comment.getTopicId());
+                    userMessage.setMsgType(1);
+                    userMessage.setTriggerMsgUserId(user.getId());
+                    userMessage.setRecvMsgUserId(user1.getId());
+                    userMessageMapper.insertSelective(userMessage);
+                }
+            }
         }
         else
         {
+            String referer = request.getHeader("Referer");
+            httpSession.setAttribute("referer",referer);
             regRespObj.setStatus(0);
             regRespObj.setAction(request.getServletContext().getContextPath()+"/user/login");
         }
         response.getWriter().println(JSON.toJSONString(regRespObj));
         return;
+    }
+    @RequestMapping("index/{cid}/{typeid}")
+    public ModelAndView index(@PathVariable Integer cid,@PathVariable Integer typeid) {
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("cid",cid);
+        modelAndView.addObject("typeid",typeid);
+        modelAndView.setViewName("jie/index");
+        return modelAndView;
     }
 }
